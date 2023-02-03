abbaa544242a0f9f35923c25d0f8b0d9bb2ec241
Vasyl Kanishchev <vasyakanishchev@gmail.com>
2023-02-03 02:27:11 +0200

    refactor: add posts to data example

--- data.js
+++ data.js
@@ -1,15 +1,22 @@
+import { compare, genSalt, hash } from "bcrypt";
+import Post from "./post.js";
+
 const data = {
     maxId: 2,
     users: [ 
         {
             id: 1,
             name: 'shtag',
-            password: 'shtag',
+            password: await hash('shtag',10),
             profilePosts: [
-                
+                new Post('https://images.pexels.com/photos/45201/kitty-cat-kitten-pet-45201.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'),
+                new Post('https://upload.wikimedia.org/wikipedia/commons/4/4d/Cat_November_2010-1a.jpg'),
+                new Post('https://www.humanesociety.org/sites/default/files/2022-08/hl-yp-cats-579652.jpg'),
+                new Post('http://www.alleycat.org/wp-content/uploads/2019/03/FELV-cat.jpg'),
             ],
             subscript: [],
             followers: [],
+            sessions: ["$2b$10$NhL.XLXwthdA4kACTPIJg."]
         }
     ],
     posts: [
