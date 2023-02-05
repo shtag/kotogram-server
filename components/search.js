395dd81ced50d8ca5ee15d113a72ec3e5a2d7697
Vasyl Kanishchev <vasyakanishchev@gmail.com>
2023-02-05 21:31:19 +0200

    feat: add searsh

---
+++ components/search.js
@@ -0,0 +1,15 @@
+import data from "../data.js";
+
+const search = (req, res) => {
+    const query = req.body.query;
+    const users = data.users.filter(user => user.username.includes(query));
+    const posts = data.posts.filter(post => post.description.includes(query));
+    console.log(users)
+    if (users) {
+        res.status(200).json({ users:users, posts: posts }).send();
+    } else {
+        res.status(404).send();
+    }
+};
+
+export { search }
