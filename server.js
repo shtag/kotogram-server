ac1b787ecc14aa454cae7a46e74ed8ed8922fbc6
Vasyl Kanishchev <vasyakanishchev@gmail.com>
2023-02-05 21:33:00 +0200

    refactor: split code to modules

--- server.js
+++ server.js
@@ -3,14 +3,17 @@ import User from './user.js';
 import data from './data.js';
 import './components/auth.js';
 import { login, isSessionActive, signup, logout } from './components/auth.js';
+import { changeLogin, changeSettings, deleteUser, getFollowers, getSubs, getUser, subscribe } from './components/users.js';
+import { addComment, getFeed, getPost, getRecomendation, like, likeComment, newPost, removeComment } from './components/posts.js';
+import { search } from './components/search.js';
 
 export const app = Express();
-app.use(Express.json())
+app.use(Express.json());
 
 export const users = data.users;
 
-
 app.get('/', (req, res) => res.json(data));
+
 // auth requests
 app.post('/signup', signup);
 app.post('/login', login);
@@ -19,32 +22,28 @@ app.post('/user/session', isSessionActive);
 
 // get list of users
 app.get('/users', (req, res) => res.json(users));
+//get user with name or id
+app.post('/user', getUser);
+app.delete('/user/:id', deleteUser);
+app.patch('/user/:id', changeLogin);
+app.patch('/user/settings/:id', changeSettings);
 
-//get user with name
-app.get('/users/:name', (req, res) => {
-    const name = req.params.name;
-    console.log(name);
-    const user = users.find(user => user.name === name)
-    if (user) {
-        res.status(200).json(user).send();
-    } else {
-        res.status(404).send()
-    }
-})
-//get user with id
-app.get('/users/:id', (req, res) => {
-    const id = req.params.id;
-    console.log(id);
-    const user = users.find(user => user.id === +id)
-    if (user) {
-        res.status(200).json(user).send();
-    } else {
-        res.status(404).send()
-    }
-})
+app.post('/post', newPost);
+app.post('/like/:id', like);
+app.post('/comment/:postId', addComment);
+app.delete('/comment/:postId', removeComment);
+
+app.get('/post', (req, res) => res.json(data.posts));
+app.get('/feed', getFeed);
+app.get('/recomendation', getRecomendation);
+app.get('/post/:id', getPost);
+app.patch('/post/:id', likeComment);
+
+app.get('/search', search);
 
 
-
-
+app.post('/subscribe', subscribe);
+app.get('/subscriptions/:id', getSubs);
+app.get('/followers/:id', getFollowers);
 
 app.listen(3000);
