f448a9260a63cde324ce11816506db8bc23a6142
Vasyl Kanishchev <vasyakanishchev@gmail.com>
2023-02-05 21:30:28 +0200

    feat: auth server

--- components/auth.js
+++ components/auth.js
@@ -1,32 +1,32 @@
-import { compare, genSalt, hash } from "bcrypt";
-import { users } from "../server.js";
-import User from "../user.js";
-
+import { compare, genSalt, hash } from 'bcrypt';
+import { users } from '../server.js';
+import User from '../user.js';
 
 const signup = async (req, res) => {
-    if (!users.find(el => el.name === req.body.name)) {
-        const user = new User(req.body.name, await hash(req.body.password, 10))
+    if (!users.find((el) => el.username === req.body.username)) {
+        const user = new User(req.body.username, await hash(req.body.password, 10));
         users.push(user);
         const response = {
-            name: req.body.name,
-            password: req.body.password
-        }
+            username: req.body.username,
+            password: req.body.password,
+        };
         res.status(200).json(response).send();
     } else {
         res.status(400).send('Login already taken');
     }
-}
+};
+
 const login = async (req, res) => {
-    const user = users.find(el => el.name === req.body.name)
+    const user = users.find((el) => el.username === req.body.username);
     if (user) {
         const isSame = await compare(req.body.password, user.password);
         if (isSame) {
             const sessionId = await genSalt(10);
             user.sessions.push(sessionId);
             const body = {
-                name: user.name,
+                username: user.username,
                 sessionId: sessionId,
-                access: true
+                access: true,
             };
             res.status(200).json(body).send();
         } else {
@@ -35,39 +35,42 @@ const login = async (req, res) => {
     } else {
         res.status(404).send('User not found');
     }
-}
+};
 
 const isSessionActive = async (req, res) => {
-    const user = users.find(el => el.name === req.body.name)
+    const user = users.find((el) => el.username === req.body.username);
     if (user) {
-        if(user.sessions.includes(req.body.sessionId)) {
+        if (user.sessions.includes(req.body.sessionId)) {
             const body = {
-                sessionActive: true
-            }
+                sessionActive: true,
+            };
             res.status(200).json(body).send();
         } else {
             const body = {
-                sessionActive: false
-            }
+                sessionActive: false,
+            };
             res.status(400).json(body).send();
         }
     } else {
         res.status(404).send('User not found');
     }
-}
+};
+
 const logout = async (req, res) => {
-    const user = users.find(el => el.name === req.body.name)
+    const user = users.find((el) => el.username === req.body.username);
     if (user) {
-        if(user.sessions.includes(req.body.sessionId)) {
+        if (user.sessions.includes(req.body.sessionId)) {
             const id = user.sessions.indexOf(req.body.sessionId);
             user.sessions.splice(id, 1);
-            res.status(200).send("logout");
+            res.status(200).send('logout');
         } else {
             res.status(400).json(body).send('Session is not active');
         }
     } else {
         res.status(404).send('User not found');
     }
-}
+};
+
+
 
 export { signup, login, isSessionActive, logout };
