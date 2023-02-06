80f6389f645ae0c120a4e28416d5559f03557913
Vasyl Kanishchev <vasyakanishchev@gmail.com>
2023-02-06 17:54:32 +0200

    feat: add data for testing

--- data.js
+++ data.js
@@ -1,31 +1,46 @@
-import { compare, genSalt, hash } from "bcrypt";
-import Post from "./post.js";
+import { compare, genSalt, hash } from 'bcrypt';
+import Post from './post.js';
+import User from './user.js';
 
 const data = {
-    maxId: 2,
-    users: [ 
+    users: [
         {
             id: 1,
-            name: 'shtag',
-            password: await hash('shtag',10),
-            profilePosts: [
-                new Post('https://images.pexels.com/photos/45201/kitty-cat-kitten-pet-45201.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'),
-                new Post('https://upload.wikimedia.org/wikipedia/commons/4/4d/Cat_November_2010-1a.jpg'),
-                new Post('https://www.humanesociety.org/sites/default/files/2022-08/hl-yp-cats-579652.jpg'),
-                new Post('http://www.alleycat.org/wp-content/uploads/2019/03/FELV-cat.jpg'),
-            ],
-            subscript: [],
-            followers: [],
-            sessions: ["$2b$10$NhL.XLXwthdA4kACTPIJg."]
-        }
+            username: 'shtag',
+            password: await hash('shtag', 10),
+            subscriptions: ['shtag4', 'shtag2', 'shtag3'],
+            sessions: ['$2b$10$NhL.XLXwthdA4kACTPIJg.'],
+            settings: {
+                photo: '',
+                language: 'en',
+                name: 'Vasyl',
+                descriptionProfile:'I am a nice cat!'
+            }
+        },
     ],
-    posts: [
-        {
-            image: 'url',
-            description: 'Nice cat'
-        }
-    ]
+    posts: [],
+};
+
+
+data.posts.push(new Post('someUrl', 'here is description', 'shtag4'))
+data.posts.push(new Post('someUrl2', 'here is description2', 'shtag3'))
+data.posts.push(new Post('someUrl1', 'here is description1', 'shtag'))
+data.posts.push(new Post('someUrl2', 'here is description2', 'shtag'))
+data.posts.push(new Post('someUrl4', 'here is description3', 'shtag'))
+for (let i = 0; i < 10; i++) {
+    setTimeout(() => {
+        data.posts.push(new Post('someUrl3', 'here is description3 shta', 'shtag2'))
+    }, 1000)
+}
+for (let i = 0; i < 10; i++) {
+    setTimeout(() => {
+        data.posts.push(new Post(`someUrl${i}`, `here is description №${i}`, 'shtag'))
+    }, 1000)
+}
+for (let i = 0; i < 10; i++) {
+    setTimeout(() => {
+        data.posts.push(new Post(`someUrl${i}`, `here is description №${i}`, 'shtag4'))
+    }, 1000)
 }
 
 export default data;
-
